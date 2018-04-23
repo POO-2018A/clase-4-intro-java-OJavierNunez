e2a6f73da1c422b16b878e38e3f5542ec4786564
OJavierNunez <38635677+OJavierNunez@users.noreply.github.com>
2018-04-22 22:35:44 -0500

    Add files via upload

---
+++ Ejercicio7/src/ejercicio7/Ejercicio7.java
@@ -0,0 +1,19 @@
+package ejercicio7;
+
+import java.util.Scanner;
+
+public class Ejercicio7 {
+
+    public static void main(String[] args) {
+        String [] oracion ={"Introduccion", "a", "POO", "con","Java"};
+        int indice = buscarPalabra(oracion, "POO");
+        System.out.println(indice);
+      
+    }
+    
+    public static int buscarPalabra(String [] stringArray, String palabra){
+
+        return -1;
+    }
+    
+}
